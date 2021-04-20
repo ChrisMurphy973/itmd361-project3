e90b66dda6882559e902fad3829ac9b0c4fe9de1
cmurphy11 <cmurphy11@hawk.iit.edu>
2021-04-20 18:13:36 -0500

    zoomed map more

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 41.9085864941437, lng: -87.65192515997421 },
-    zoom: 12,
+    zoom: 18,
   });
 
   //html stuff here
