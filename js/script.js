c7b073f1776b3dcf12c0bfb5e4991887bd5aebab
cmurphy11 <cmurphy11@hawk.iit.edu>
2021-04-20 18:32:25 -0500

    testing some map additions, added png

--- js/script.js
+++ js/script.js
@@ -7,11 +7,26 @@ function initMap() {
   });
 
   //html stuff here
-
+  const IOInfo = '<div class="googleMapsInfo"><h1>IO Theatre</h1><p>While it is currently closed (due to COVID), the IO Theatre was one of my favorite places to go for entertainment and comedy in Chicago.</p></div>';
   //icons
-
+  const mask = {
+    url: "images/mask.png",
+    scaledSize: new google.maps.Size(36, 36)
+  };
   //markers
-
+  const IOTheatre = new google.maps.Marker ({
+    position: {lat: 41.90855147775848, lng: -87.65192224479192},
+    map,
+    icon: mask,
+    animation: google.maps.Animation.DROP,
+    title: "IO Theatre",
+  });
+  //windows
+  const IOTheatreWindow = new google.maps.InfoWindow ({
+    content: IOInfo,
+  })
   //click listeners
-  
+  IOTheatre.addListener("click", () => {
+    IOTheatreWindow.open(map, IOTheatre);
+  })
 }
