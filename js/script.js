ccee2070ac5c3163228a26f08074b576aa7de1e8
cmurphy11 <cmurphy11@hawk.iit.edu>
2021-04-20 18:22:48 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ var map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 41.9085864941437, lng: -87.65192515997421 },
-    zoom: 18,
+    center: { lat: 41.90855147775848, lng: -87.65192224479192 },
+    zoom: 20,
   });
 
   //html stuff here
